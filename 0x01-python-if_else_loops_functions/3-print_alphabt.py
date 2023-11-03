edc21fc8905754e0d9c8dd935840f232696e31bb
Shobande-Israel08 <israelshobande08@gmail.com>
2023-11-03 08:18:15 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-    if (character != 101 and character != 113):
+for character in range(87, 113):
+    if (character != 81 and character != 93):
         print("{:c}".format(character), end='')
